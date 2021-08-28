56758aa18bd7eeed73651d215fed6c3b01db69a6
Walisson <40742446+WalissonCF@users.noreply.github.com>
2021-08-28 14:00:17 -0300

    Update OwnerController.java
    Adicionado notação CrossOrigin para permitir acesso ao recursos do servidor

--- src/main/java/br/com/mysafeestablishmentcompany/controller/OwnerController.java
+++ src/main/java/br/com/mysafeestablishmentcompany/controller/OwnerController.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin; //TODO: Verificar se a importação está correta
 
+@CrossOrigin
 @RestController
 public class OwnerController {
 
