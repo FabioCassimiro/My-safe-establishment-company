85cc9eace18d28828e4122c28a8b97fb4a24e842
Fabio Cassimiro <fabiocassimiro.project@gmail.com>
2021-08-29 14:15:45 -0300

    MSEC-20210829 - Refatoração das classes de owner e adição de crossOrigin

--- src/main/java/br/com/mysafeestablishmentcompany/controller/OwnerController.java
+++ src/main/java/br/com/mysafeestablishmentcompany/controller/OwnerController.java
@@ -5,29 +5,28 @@ import br.com.mysafeestablishmentcompany.domain.Owner;
 import br.com.mysafeestablishmentcompany.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/public/owner")
+@CrossOrigin
 public class OwnerController {
 
-    OwnerService ownerService;
+    private final OwnerService ownerService;
 
     @Autowired
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
 
-    @PostMapping("/public/owner/register")
-    public ResponseEntity createCompany(@RequestBody RegisterCompany registerCompany) {
+    @PostMapping("/register")
+    public ResponseEntity<String> createCompany(@RequestBody RegisterCompany registerCompany) {
         return ownerService.register(registerCompany);
     }
 
-    @PostMapping("/public/owner/login")
-    public ResponseEntity login(@RequestBody Owner owner) throws Exception {
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody Owner owner) throws Exception {
         return ownerService.login(owner);
     }
-
 
 }
