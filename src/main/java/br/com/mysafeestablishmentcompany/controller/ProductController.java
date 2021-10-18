202a788283321d2c4c9330cd49aa0e11388c58e4
Fabio Cassimiro <fabiocassimiro.project@gmail.com>
2021-10-18 19:47:02 -0300

    MSEC-20211018 - ajuste de lançamento de exceptions

--- src/main/java/br/com/mysafeestablishmentcompany/controller/ProductController.java
+++ src/main/java/br/com/mysafeestablishmentcompany/controller/ProductController.java
@@ -1,6 +1,7 @@
 package br.com.mysafeestablishmentcompany.controller;
 
 import br.com.mysafeestablishmentcompany.domain.Product;
+import br.com.mysafeestablishmentcompany.exception.ProductNotFoundException;
 import br.com.mysafeestablishmentcompany.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,27 +21,27 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public ArrayList<Product> allProducts(){
+    public ArrayList<Product> allProducts() throws Exception {
         return productService.allProducts();
     }
 
     @GetMapping("/product/{id}")
-    public Product productById(@PathVariable() Long id){
+    public Product productById(@PathVariable() Long id) throws ProductNotFoundException {
         return productService.product(id);
     }
 
     @PostMapping("product/register")
-    public Product registerProduct(@RequestBody Product product){
+    public Product registerProduct(@RequestBody Product product) throws Exception {
         return productService.register(product);
     }
 
     @DeleteMapping("/product/delete/{id}")
-    public String delectProduct(@PathVariable() Long id){
+    public String delectProduct(@PathVariable() Long id) throws Exception {
         return productService.delete(id);
     }
 
     @PutMapping("/product/update")
-    public Product updateProduct(@RequestBody Product product){
+    public Product updateProduct(@RequestBody Product product) throws Exception {
         return productService.update(product);
     }
 
